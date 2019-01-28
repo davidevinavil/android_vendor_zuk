yuweiyuan8/proprietary_vendor_xiaomi

msm8996-common: Add MIUI offmode charging daemon
Change-Id: I0e0bdfc260cedb3ded6ddc457c52129c90a58877

msm8996-common/Android.mk
@@ -168,4 +168,14 @@ LOCAL_MODULE_SUFFIX := .xml
 LOCAL_VENDOR_MODULE := true
 include $(BUILD_PREBUILT)
 
+include $(CLEAR_VARS)
+LOCAL_MODULE := chargeonlymode
+LOCAL_MODULE_OWNER := xiaomi
+LOCAL_MODULE_PATH := $(TARGET_ROOT_OUT_SBIN)
+LOCAL_UNSTRIPPED_PATH := $(TARGET_ROOT_OUT_SBIN_UNSTRIPPED)
+LOCAL_SRC_FILES := proprietary/rootfs/sbin/chargeonlymode
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := EXECUTABLES
+include $(BUILD_PREBUILT)
+
 endif

davidevinavil/android_vendor_zuk

msm8996-common/Android.mk
@@ -148,4 +148,14 @@ LOCAL_MODULE_SUFFIX := .apk
 LOCAL_VENDOR_MODULE := true
 include $(BUILD_PREBUILT)
 
+include $(CLEAR_VARS)
+LOCAL_MODULE := chargeonlymode
+LOCAL_MODULE_OWNER := zuk
+LOCAL_MODULE_PATH := $(TARGET_ROOT_OUT_SBIN)
+LOCAL_UNSTRIPPED_PATH := $(TARGET_ROOT_OUT_SBIN_UNSTRIPPED)
+LOCAL_SRC_FILES := proprietary/rootfs/sbin/chargeonlymode
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := EXECUTABLES
+include $(BUILD_PREBUILT)
+
 endif